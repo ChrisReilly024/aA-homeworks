a92b04c242f5cc79fc5a181ec2a8deb06460af80
ChrisReilly024 <creilly024@gmail.com>
2020-04-03 07:18:47 -0400

    queue probs completed

--- w3d5/diy adt.rb
+++ w3d5/diy adt.rb
@@ -23,6 +23,25 @@
 # To test that your code works, create a new instance of the Stack class, and play around with adding and removing elements. Remember, a stack follows the principle of LIFO!
 
 # Exercise 2 - Queue
+
+class Queue
+    attr_reader :array
+    def initialize
+        @array = []
+    end
+
+    def enqueue(el)
+        array.unshift(el)
+    end
+
+    def dequeue
+        array.shift(el)
+    end
+
+    def peek
+        array.last
+    end
+end
 # Now let's write a Queue class. We will need the following instance methods: enqueue(el), dequeue, and peek.
 
 # Test your code to ensure it follows the principle of FIFO.
